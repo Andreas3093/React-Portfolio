df2dd77c6bd8b410408d05d5afeca1c4b155c956
AndreasIrta <andreasirta0711@gmail.com>
2021-07-30 00:08:29 -0600

    Updates

---
+++ precache-manifest.7e469ba96351d2c9899f40fe5baab727.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678a3d15cb6465b61ede6d8780811bc3",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "853baf8914a334a265c2",
+    "url": "/React-Portfolio/static/css/2.de974f25.chunk.css"
+  },
+  {
+    "revision": "3c1a20db5fbeffb84076",
+    "url": "/React-Portfolio/static/css/main.7ba0fc56.chunk.css"
+  },
+  {
+    "revision": "853baf8914a334a265c2",
+    "url": "/React-Portfolio/static/js/2.f09c4839.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/React-Portfolio/static/js/2.f09c4839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c1a20db5fbeffb84076",
+    "url": "/React-Portfolio/static/js/main.fb10c272.chunk.js"
+  },
+  {
+    "revision": "53cd5964469ae1da263b",
+    "url": "/React-Portfolio/static/js/runtime-main.01560ae1.js"
+  },
+  {
+    "revision": "d3faaf3567c4b81e47274cffa8779b85",
+    "url": "/React-Portfolio/static/media/Andreas_Irta_Resume.d3faaf35.PDF"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/React-Portfolio/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/React-Portfolio/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/React-Portfolio/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/React-Portfolio/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/React-Portfolio/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/React-Portfolio/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/React-Portfolio/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/React-Portfolio/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/React-Portfolio/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/React-Portfolio/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/React-Portfolio/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/React-Portfolio/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/React-Portfolio/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/React-Portfolio/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/React-Portfolio/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/React-Portfolio/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/React-Portfolio/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
